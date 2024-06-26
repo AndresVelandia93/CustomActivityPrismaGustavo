6849dac1011873559f17e1333d91efd7da79748d
AndresVelandia93 <andresvelandia93@hotmail.com>
2024-06-26 03:35:23 -0500

    Validacion Final

--- server/JwtDecoder.js
+++ server/JwtDecoder.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 
 function decodeJwt(token, secret) {
   try {
-    console.log('Decodificacion JWT')
+    //console.log('Decodificacion JWT')
     return jwt.verify(token, secret, { algorithms: ['HS256'] }); // specify the algorithm
 
   } catch (err) {
-    console.error('Failed to decode JWT:', err);
+    //console.error('Failed to decode JWT:', err);
     return null;
   }
 }
