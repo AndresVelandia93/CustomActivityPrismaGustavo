3be3c48796abddec6bee898a07e113994c9514bd
Andres Velandia <andresvelandia93@hotmail.com>
2024-08-06 01:19:38 -0500

    InicioProyecto_vf2

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.post('/save', (req, res) => {
     console.log('Save route');
+    console.log(req.toString());
     const decoded = decodeJwt(req.body.toString('utf8'), secret);
     console.log('Decoded JWT:', decoded);
     res.status(200).send('Save');
